1a597e0a93b202163a1d537f70c24896c0d2d6f6
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-11-23 03:43:00 +0600

    updated portfolio

---
+++ README.md
@@ -0,0 +1,9 @@
+## Portfolio of Imdadul Ahmed
+
+Passionate Software Engineer with a background in Nuclear Engineering (Bachelors) and Business Administration (Masters), backed by a bootcamp certification. Eager to bring my unique blend of technical skills and strategic thinking to a dynamic role. Committed to driving innovation and efficiency, I aim to contribute meaningfully to projects and embrace the challenges of the ever-evolving software development landscape.
+
+![landing page of the portfolio](resume-screenshot.png)
+
+### Check out my portfolio: [https://ahmedimdad93.netlify.app](https://ahmedimdad93.netlify.app/)
+
+### Download Resume: [Drive link here](https://drive.google.com/file/d/1_IXevhEB3jwBzlxtv7exDOiZMph97IZ7/view?usp=sharing)
---
+++ src/Components/Contact.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+
+const Contact = ({ data }) => {
+  const [url, setUrl] = useState(
+    "mailto:test@example.com?subject=subject&body=body"
+  );
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  // console.log(data);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_yuz2hyf",
+        "template_05gdbit",
+        e.target,
+        "user_CpGoLcIpWbGuprk1X6211"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    setName("");
+    setSubject("");
+    setEmail("");
+    setMessage("");
+    setSuccess(true);
+  };
+
+  return (
+    <section id="contact">
+      <div className="row section-head">
+        <div className="two columns header-col">
+          <h1>
+            <span>Get In Touch.</span>
+          </h1>
+        </div>
+
+        <div className="ten columns">
+          <p className="lead">{data?.message}</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="eight columns">
+          <form onSubmit={handleClick} id="contactForm" name="contactForm">
+            <fieldset>
+              <div>
+                <label htmlFor="contactName">
+                  Name <span className="required">*</span>
+                </label>
+                <input
+                  value={name}
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactName"
+                  name="contactName"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactEmail">
+                  Email <span className="required">*</span>
+                </label>
+                <input
+                  value={email}
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactEmail"
+                  name="contactEmail"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactSubject">Subject</label>
+                <input
+                  value={subject}
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactSubject"
+                  name="contactSubject"
+                  onChange={(e) => setSubject(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactMessage">
+                  Message <span className="required">*</span>
+                </label>
+                <textarea
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  cols="50"
+                  rows="15"
+                  id="contactMessage"
+                  name="contactMessage"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit" className="submit">
+                  Submit
+                </button>
+                <span id="image-loader">
+                  <img alt="" src="images/loader.gif" />
+                </span>
+              </div>
+            </fieldset>
+          </form>
+
+          {success && (
+            <div id="message-success">
+              <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
+            </div>
+          )}
+        </div>
+
+        <aside className="four columns footer-widgets">
+          <div className="widget widget_contact">
+            <h4>Address and Phone</h4>
+            <p className="address">
+              {data?.name}
+              <br />
+              {data?.address.street} <br />
+              {data?.address.city}, {data?.address.state} {data?.address.zip}
+              <br />
+              <span>{data?.phone}</span>
+            </p>
+          </div>
+
+          <div className="widget widget_tweets"></div>
+        </aside>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
+
+// <div id="message-warning"> Error boy</div>
